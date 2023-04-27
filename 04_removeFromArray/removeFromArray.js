cb634ac8b0f52ec7908011980e3b27fca06e9392
mpetkov228 <mpetkov228@gmail.com>
2023-04-27 13:23:26 +0300

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let arrCopy = arr.slice();
+    for (let arg of args) {
+        let i = arrCopy.indexOf(arg);
+        if (i != -1) {
+            arrCopy.splice(i, 1);
+        }
+    }
 
-};
-
+    return arrCopy;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
