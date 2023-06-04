2f3f7748c303b45368a1836d33a1964c9b1507c2
mpetkov228 <mpetkov228@gmail.com>
2023-06-04 12:51:54 +0300

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	if (numbers.length == 0) {
+    return 0;
+  }
+
+  return numbers[0] + sum(numbers.slice(1));
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  if (numbers.length == 0) {
+    return 1;
+  }
 
+  return numbers[0] * multiply(numbers.slice(1));
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0 || num == 1) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
