9ba24bceda449276924ee416030fb7f0b4c91c25
mpetkov228 <mpetkov228@gmail.com>
2023-06-05 20:55:04 +0300

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const peopleCopy = people.slice();
+    peopleCopy.sort((a, b) => {
+        const currentYear = new Date().getFullYear();
+        const personAAge = a.yearOfDeath ? a.yearOfDeath - a.yearOfBirth : currentYear - a.yearOfBirth;
+        const personBAge = b.yearOfDeath ? b.yearOfDeath - b.yearOfBirth : currentYear - b.yearOfBirth;
 
+        return personBAge - personAAge;
+    })
+    return peopleCopy[0];
 };
 
 // Do not edit below this line
