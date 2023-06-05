f3534ae38ba45995465839e9d7d18dfbbcd179fb
mpetkov228 <mpetkov228@gmail.com>
2023-06-05 20:32:52 +0300

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
 
+    if (n < 0) {
+        return 'OOPS';
+    }
+
+    if (n == 0) {
+        return 0;
+    }
+
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < n; i++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
